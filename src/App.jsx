1829aec9cc7bd7e51bab18b9e23ebfc1f0d89c1b
Sebastian Sanabria <fssanabriac@gmail.com>
2021-10-26 20:50:11 -0500

    Added some examples s of first components (reusable html code)

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        This is the header
+      </header>
+      <main>
+        This is the main section
+        <Hello name="Marge" />
+        <Hello name='Homer' />
+        <Hello name='Bart' />
+        <Hello name='Lisa' />
+      </main>
+      <footer>
+        And this is the footer
+      </footer>
+    </div>
+  );
+}
+
+function Hello(props){
+  return(
+    <div>Hola {props.name}</div>
+  );
+}
+
+export default App;
