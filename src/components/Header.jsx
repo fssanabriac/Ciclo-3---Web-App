ce16ce3a0d8c58bfc7409d7b6e0a46e3a5056090
Sebastian Sanabria <fssanabriac@gmail.com>
2021-10-28 17:29:53 -0500

    Added icon, name, register and login text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,17 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBook} from '@fortawesome/free-solid-svg-icons';
 
 function Header(){
     return(
-        <div>
-            <h3>
-                This is the header.
-            </h3>
-            <hr />
+        <div className='Header'>
+            <div>
+                <FontAwesomeIcon icon={faBook} />
+                <span className='Header__Icon'>Enterprise</span>
+            </div>
+            <div >
+                <span className='Header__span'>Registrarse</span>
+                <span className='Header__span'>Ingresar</span>
+            </div>
         </div>
     );
 }
