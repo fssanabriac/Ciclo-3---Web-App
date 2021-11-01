74b1a7dcd3e1e2224d1ef0160613cfc765409dc2
Sebastian Sanabria <fssanabriac@gmail.com>
2021-11-01 15:49:26 -0500

    Created sells.jsx products.jsx and users.jsx inside pages/private

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,21 +1,39 @@
+import {Link} from 'react-router-dom'
 
 function Login(){
     return(
-        <div>
-            <h1>This is the login page</h1>
-            <h3>Inicia Sesión</h3>
-            <form action="" className='form'>
-                <div>
-                    <input type="email" required />
-                    <input type="password" required />
+        <div className='Login__container'>
+            <h2 className='login-title'>Inicia Sesión</h2>
+            <form action="" className='Login__form'>
+                <div className='Login__inputs'>
+                        <input type="email" placeholder='Correo Electrónico' required />
+                        <input type="password" placeholder='Contraseña' required />
                 </div>
-                <div>
+                <div className='Login__rememberForget'>
                     <label htmlFor='remember_me'>
                         <input type="checkbox" name='remember_me'/>
                         Remember me
                     </label>
+                    <Link className='Login__rememberForget__Link' to='/recover'>
+                        <div>Olvide mi Contraseña</div>
+                    </Link>
                 </div>
-                <button type='submit'>Login</button>
+                <div>
+                    <Link to='/admin'>
+                        <button className='Login__submit' type='submit'>Inicia Sesión</button>
+                    </Link>
+                </div>
+                <div className='Login__register'>
+                    <span>¿No tienes cuenta?</span> 
+                    <Link className='Login__register__Link' to='/sign-in'>
+                        <div>Regístrate</div>
+                    </Link>
+                </div>
+                <div >
+                    <button className='Login__Google'>
+                        Continua con Google
+                    </button>
+                </div>
             </form>
         </div>
     );
