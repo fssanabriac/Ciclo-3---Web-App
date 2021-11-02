00a5ceac55d79045dc7947afbba442c3c99f7346
Sebastian Sanabria <fssanabriac@gmail.com>
2021-11-02 17:49:35 -0500

    added functionality to sells page: table and form

--- src/pages/private/sells.jsx
+++ src/pages/private/sells.jsx
@@ -3,6 +3,41 @@ import React, {useEffect, useState} from 'react'
 const Sells = () => {
     const [verListaVentas, setVerListaVentas] = useState(true);
     const [textoBoton, setTextoBoton] = useState('Registrar venta');
+    const ventas = [
+        {
+            idVenta:"1",
+            valorTotal:"20000",
+            idProducto:"1",
+            cantidad:"4",
+            valorUnidad:"5",
+            fecha:"10/10/2021",
+            idComprador:"123",
+            nombreComprador:"John Doe",
+            nombreVendedor:"Homer Simpson",
+        },
+        {
+            idVenta:"1",
+            valorTotal:"20000",
+            idProducto:"1",
+            cantidad:"4",
+            valorUnidad:"5",
+            fecha:"10/10/2021",
+            idComprador:"123",
+            nombreComprador:"John Doe",
+            nombreVendedor:"Homer Simpson",
+        },
+        {
+            idVenta:"1",
+            valorTotal:"20000",
+            idProducto:"1",
+            cantidad:"4",
+            valorUnidad:"5",
+            fecha:"10/10/2021",
+            idComprador:"123",
+            nombreComprador:"John Doe",
+            nombreVendedor:"Homer Simpson",
+        }
+    ]
 
     useEffect( (e) => {
         if (verListaVentas) {
@@ -18,29 +53,120 @@ const Sells = () => {
             <button onClick={() => {setVerListaVentas(!verListaVentas)}}>
                 {textoBoton}
             </button>
-            { verListaVentas ? <MostrarVentas/> : <ModificarVentas/>}
+            { verListaVentas ? <MostrarVentas listaVentas={ventas}/> : <ModificarVentas/>}
         </div>
     )
 }
 
 export default Sells
 
-const MostrarVentas = () =>{
-    return <div>Esta es la lista de ventas</div> 
+const MostrarVentas = ({listaVentas}) =>{
+    return <div>
+        Esta es la lista de ventas
+        <table>
+            <thead>
+                <tr>
+                    <th>Id Venta</th>
+                    <th>Valor Total</th>
+                    <th>Id Producto</th>
+                    <th>Cantidad</th>
+                    <th>Valor unidad</th>
+                    <th>Fecha de venta</th>
+                    <th>Id Comprador</th>
+                    <th>Nombre Comprador</th>
+                    <th>Nombre vendedor</th>
+                </tr>
+            </thead>
+            <tbody>
+                {listaVentas.map((venta)=> {
+                    return <tr>
+                        <td>{venta.idVenta}</td>
+                        <td>{venta.valorTotal}</td>
+                        <td>{venta.idProducto}</td>
+                        <td>{venta.cantidad}</td>
+                        <td>{venta.valorTotal}</td>
+                        <td>{venta.fecha}</td>
+                        <td>{venta.idComprador}</td>
+                        <td>{venta.nombreComprador}</td>
+                        <td>{venta.nombreVendedor}</td>
+                    </tr>
+                }
+                )}
+            </tbody>
+        </table>
+    </div>
+
 }
 
 const ModificarVentas = () =>{
+    const tags =[
+        {
+            key:1,
+            htmlForOption:"sellId",
+            label: "Id de la venta",
+            typeOption:"number"
+        },
+        {
+            key:2,
+            htmlForOption:"total",
+            label:"valor total",
+            typeOption:"number"
+        },
+        {
+            key:3,
+            htmlForOption:"productId",
+            label:"Id del producto",
+            typeOption:"number"
+        },
+        {
+            key:4,
+            htmlForOption:"quantity",
+            label:"Cantidad",
+            typeOption:"number"
+        },
+        {
+            key:5,
+            htmlForOption:"unitPrice",
+            label:"Valor unidad",
+            typeOption:"number"
+        },
+        {
+            key:6,
+            htmlForOption:"sellDate",
+            label:"Fecha de venta",
+            typeOption:"date"
+        },
+        {
+            key:7,
+            htmlForOption:"idBuyer",
+            label:"Id del comprador",
+            typeOption:"number"
+        },
+        {
+            key:8,
+            htmlForOption:"nameBuyer",
+            label:"Nombre del comprador",
+            typeOption:"text"
+        },
+        {
+            key:9,
+            htmlForOption:"nameSeller",
+            label:"Nombre del vendedor",
+            typeOption:"text"
+        },
+    ]
+
     return <div>Aquí se crea o modifica una venta
-        <ul>
-            <li>Identificador Venta</li>
-            <li>Valor total</li>
-            <li>identificador producto</li>
-            <li>Cantidad Producto</li>
-            <li>Precio Unitario</li>
-            <li>Fecha venta</li>
-            <li>Documento id cliente</li>
-            <li>Nombre Cliente</li>
-            <li>Vendedor</li>
-        </ul>
+        <form >
+            {tags.map(
+                (tag)=>{
+                return <label key={tag.key} htmlFor={tag.htmlForOption}>
+                    {tag.label}
+                    <input type={tag.typeOption} />
+                </label>
+                }
+            )
+            }
+        </form>
     </div>
 }
