a29f23fd5995144298bb9d2dfbcaaac84c575203
Sebastian Sanabria <fssanabriac@gmail.com>
2021-11-13 11:49:52 -0500

    Connected users page with backend

--- src/pages/private/users.jsx
+++ src/pages/private/users.jsx
@@ -1,53 +1,78 @@
 import React, {useEffect, useState, useRef} from 'react'
-import { faCheckSquare, faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faCheckSquare, faEdit, faTimesCircle, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { nanoid } from 'nanoid';
 import axios from 'axios';
-import { toast } from 'react-toastify';
+import { toast, ToastContainer } from 'react-toastify';
 
 const Users = () => {
-    const usuarios = [
-        {
-            nombre:"Homer",
-            id:1,
-            rol:"Administrador",
-            estado:"Autorizado"
-        },
-        {
-            nombre:"Bart",
-            id:2,
-            rol:"Vendedor",
-            estado:"Pendiente"
-        },
-        {
-            nombre:"Marge",
-            id:3,
-            rol:"Administrador",
-            estado:"Autorizado"
-        },
-        {
-            nombre:"Lisa",
-            id:4,
-            rol:"Vendedor",
-            estado:"No Autorizado"
-        },
-        {
-            nombre:"Maggie",
-            id:5,
-            rol:"Administrador",
-            estado:"Pendiente"
-        },
-    ]
+    // const usuarios = [
+    //     {
+    //         nombre:"Homer",
+    //         id:1,
+    //         rol:"Administrador",
+    //         estado:"Autorizado"
+    //     },
+    //     {
+    //         nombre:"Bart",
+    //         id:2,
+    //         rol:"Vendedor",
+    //         estado:"Pendiente"
+    //     },
+    //     {
+    //         nombre:"Marge",
+    //         id:3,
+    //         rol:"Administrador",
+    //         estado:"Autorizado"
+    //     },
+    //     {
+    //         nombre:"Lisa",
+    //         id:4,
+    //         rol:"Vendedor",
+    //         estado:"No Autorizado"
+    //     },
+    //     {
+    //         nombre:"Maggie",
+    //         id:5,
+    //         rol:"Administrador",
+    //         estado:"Pendiente"
+    //     },
+    // ]
     const [verListaUsuarios, setVerListaUsuarios] = useState(true);
     const [textoBotonUsuarios, setTextoBotonUsuarios] = useState('Actualizar rol de usuario')
+    const [usuarios, setUsuarios] = useState([]);
+    const [consultarBackEnd, setConsultarBackEnd] = useState(true);
 
     useEffect(() =>{
+
         if(verListaUsuarios){
-            setTextoBotonUsuarios('Actualizar rol de usuario');
+            setTextoBotonUsuarios('Crear de usuario');
         }else{
             setTextoBotonUsuarios('Ver lista de usuarios');
+            setConsultarBackEnd(true);
         }
+
     },[verListaUsuarios]);
+
+    useEffect(() => {
+        
+        const recibirDatosBackEnd = () =>{
+            const options = { method: 'GET', url: 'http://localhost:5000/usuarios' };
+
+            axios.request(options).then(function (response) {
+                console.log(response.data);
+                setUsuarios(response.data)
+            }).catch(function (error) {
+                console.error(error);
+            });
+        }
+
+        if (consultarBackEnd){
+            recibirDatosBackEnd();
+            setConsultarBackEnd(false);
+        }
+    }, [consultarBackEnd])
+
     return (
         <div>
             <div className='Products__title'> {/* Contenedor de titulo y boton que cambia estado de tabla*/}
@@ -55,13 +80,17 @@ const Users = () => {
                 <button className='Products__title__button' onClick={(e) => { setVerListaUsuarios(!verListaUsuarios) }}>{textoBotonUsuarios}</button>
             </div>
             <div className='abc'>
-                {!verListaUsuarios ? <ActualizarRol /> : <ListaUsuarios listaUsuarios={usuarios} />}
+                {!verListaUsuarios ? 
+                    <ActualizarRol setConsultarBackEnd={setConsultarBackEnd} />
+                :
+                    <ListaUsuarios usuariosBackEnd={usuarios} setConsultarBackEnd={setConsultarBackEnd} />}
             </div>
+            <ToastContainer position="bottom-right" autoClose={5000}/>
         </div>
     )
 }
 
-const ListaUsuarios = ({listaUsuarios}) => {
+const ListaUsuarios = ({usuariosBackEnd, setConsultarBackEnd}) => {
     const [editUser, setEditUser] = useState(false);
 
     return <div className='Products__container-table'>
@@ -76,35 +105,157 @@ const ListaUsuarios = ({listaUsuarios}) => {
                 </tr>
             </thead>
             <tbody>
-                {listaUsuarios.map((usuario)=>{
-                    return <tr key={usuario.id}>
-                        <td>{usuario.nombre}</td>
-                        <td>{usuario.id}</td>
-                        <td>{usuario.rol}</td>
-                        <td>{usuario.estado}</td>
-                        <td className='Products__table__icons'>
-                            {editUser ?
-                                <FontAwesomeIcon
-                                    onClick={() => {
-                                        // actualizarUser();
-                                        setEditUser(!editUser)
-                                    }
-                            } className='Products__table__confirm-button' icon={faCheckSquare} />
-                                :
-                                <FontAwesomeIcon
-                                    onClick={() => setEditUser(!editUser)} className='Products__table__edit' icon={faEdit} />
-                            }
-                            <FontAwesomeIcon className='Products__table__remove' icon={faTrash} />
-                        </td>
-                    </tr>
-                })}
-
+                    {usuariosBackEnd.map((usuario) => {
+                        return <FilaUsuario key={nanoid()} usuario={usuario} setConsultarBackEnd={setConsultarBackEnd}/>
+                    })
+                    }
             </tbody>
         </table>
         </div>
 }
 
-const ActualizarRol = () => {
+const FilaUsuario = ({usuario, setConsultarBackEnd})=>{
+    const [infoNuevoUsuario,setInfoNuevoUsuario] = useState({
+        name : usuario.name,
+        idNumber : usuario.idNumber,
+        role : usuario.role,
+        status : usuario.status
+    });
+    const [editUser, setEditUser] = useState(false);
+    
+    useEffect(() => {
+        return console.log('Editando registro: ' + editUser)
+    }, [editUser])
+
+
+    const actualizarUsuario = async () => {
+        console.log(infoNuevoUsuario);
+        // enviar al BackEnd.
+
+        const options = {
+            method: 'PATCH',
+            url: `http://localhost:5000/usuarios/${usuario._id}`,
+            headers: {'Content-Type': 'application/json'},
+            data: {
+                ...infoNuevoUsuario,
+                id : infoNuevoUsuario._id,
+                name : infoNuevoUsuario.name,
+                idNumber : infoNuevoUsuario.idNumber,
+                role : infoNuevoUsuario.role,
+                status : infoNuevoUsuario.status
+            }
+            // data: {state: 'No Disponible', description: 'LIBRO 5', price: 20000}
+          };
+          
+          await axios.request(options).then(function (response) {
+            console.log(response.data);
+            toast.success('Usuario actualizado con exito.')
+            setConsultarBackEnd(true);
+          }).catch(function (error) {
+            console.error(error);
+            toast.error('Usuario no pudo ser actualizado.')
+          });
+    };
+
+
+    const eliminarUsuario = async() =>{
+        const options = {
+            method: 'DELETE',
+            url: `http://localhost:5000/usuarios/${usuario._id}`,
+            headers: {'Content-Type': 'application/json'},
+            data:{...infoNuevoUsuario, id: usuario._id}
+          };
+          
+          axios.request(options).then(function (response) {
+            console.log(response.data);
+            toast.success('Usuario eliminado exitosamente.')
+            setConsultarBackEnd(true);
+          }).catch(function (error) {
+            console.error(error);
+            toast.error('Error eliminando Usuario.')
+          });
+    }
+
+    return <tr>
+        {editUser ? 
+            <>
+                <td>
+                    <input
+                        type="text"
+                        defaultValue={usuario.nombre}
+                        onChange={(e) => setInfoNuevoUsuario({ setInfoNuevoUsuario, name: e.target.value })} 
+                    />
+                </td>
+                <td>
+                    <input
+                        type="text"
+                        defaultValue={usuario.id}
+                        onChange={(e) => setInfoNuevoUsuario({ ...infoNuevoUsuario, idNumber: e.target.value })} 
+                    />
+                </td>
+                <td>
+                    <select 
+                        defaultValue={0}
+                        name="role" 
+                        onChange={(e) => setInfoNuevoUsuario({ ...infoNuevoUsuario, role: e.target.value })}
+                    >
+                        <option disabled value={0}>Seleccione un rol</option>
+                        <option>Administrador</option>
+                        <option>Vendedor</option>
+                    </select>
+                </td>
+                <td>
+                    <select 
+                        defaultValue={0} 
+                        name="status"
+                        onChange={(e) => setInfoNuevoUsuario({ ...infoNuevoUsuario, status: e.target.value })}
+                    >
+                        <option disabled value={0}>Seleccione un estado</option>
+                        <option>Pendiente</option>
+                        <option>No Autorizado</option>
+                        <option>Autorizado</option>
+                    </select>
+                </td>
+                <td className='Products__table__icons'>
+                    <FontAwesomeIcon 
+                        onClick={() => {
+                            actualizarUsuario();
+                            setEditUser(!editUser)
+                        }
+                            } className='Products__table__confirm-button' icon={faCheckSquare} />
+                    <FontAwesomeIcon
+                        onClick={() => {
+                            setEditUser(!editUser)
+                        }} 
+                        className='Products__table__cancel-edit' icon={faTimesCircle} 
+                    />
+            </td>
+            </>
+        :
+            <>
+                <td>{usuario.name}</td>
+                <td>{usuario.idNumber}</td>
+                <td>{usuario.role}</td>
+                <td>{usuario.status}</td>
+                <td className='Products__table__icons'>
+                    <FontAwesomeIcon
+                        onClick={() => setEditUser(!editUser)} 
+                        className='Products__table__edit' icon={faEdit} 
+                    />
+                    <FontAwesomeIcon 
+                        onClick={() => {
+                            eliminarUsuario();
+                        }} 
+                        className='Products__table__remove' icon={faTrash}
+                    />
+                </td>
+            </>
+        }
+    </tr>
+
+}
+
+const ActualizarRol = ({setConsultarBackEnd}) => {
     const usersTagAttributes = [
         {
             htmlForOption: "name",
@@ -144,6 +295,7 @@ const ActualizarRol = () => {
           
         await axios.request(options).then(function (response) {
             console.log(response.data);
+            setConsultarBackEnd(true);
             toast.success('Usuario creado exitosamente.');
             // setConsultarTabla(true);
         }).catch(function (error) {
