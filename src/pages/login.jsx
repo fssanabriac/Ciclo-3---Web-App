b069c20a65bfd5cfd6f3bc4cecb0cac1ea5a5004
Yerson <yersonsuarez@usantotomas.edu.co>
2021-11-15 10:48:47 -0500

    Validación de formulario vacio

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,48 +1,81 @@
-import Logo from 'components/logo';
-import {Link} from 'react-router-dom'
+import Logo from "components/logo";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-function Login(){
-    return(
-        <div className='Login__container'>
-            <div className= 'Register_login_header'>
-                <Link className='Register_login__logo' to="/">
-                    <Logo />
-                </Link>
-            </div>
-            <h2 className='login-title'>Inicia Sesión</h2>
-            <form action="" className='Login__form'>
-                <div className='Login__inputs'>
-                        <input type="email" placeholder='Correo Electrónico' required />
-                        <input type="password" placeholder='Contraseña' required />
-                </div>
-                <div className='Login__rememberForget'>
-                    <label htmlFor='remember_me'>
-                        <input type="checkbox" name='remember_me'/>
-                        Remember me
-                    </label>
-                    <Link className='Login__rememberForget__Link' to='/recover'>
-                        <div>Olvide mi Contraseña</div>
-                    </Link>
-                </div>
-                <div>
-                    <Link to='/admin'>
-                        <button className='Login__submit' type='submit'>Inicia Sesión</button>
-                    </Link>
-                </div>
-                <div className='Login__register'>
-                    <span>¿No tienes cuenta?</span> 
-                    <Link className='Login__register__Link' to='/sign-in'>
-                        <div>Regístrate</div>
-                    </Link>
-                </div>
-                <div >
-                    <button className='Login__Google'>
-                        Continua con Google
-                    </button>
-                </div>
-            </form>
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`Your state values: \n 
+            email: ${email} \n 
+            password: ${password} \n 
+            You can replace this alert with your process`);
+  };
+  return (
+    <div className="Login__container">
+      <div className="Register_login_header">
+        <Link className="Register_login__logo" to="/">
+          <Logo />
+        </Link>
+      </div>
+      <h2 className="login-title">Inicia Sesión</h2>
+      <form action="" className="Login__form" onSubmit={handleSubmit}>
+        <div className="Login__inputs">
+          <input
+            type="email"
+            placeholder="Correo Electrónico"
+            onChange={handleEmailChange}
+            value={email}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Contraseña"
+            onChange={handlePasswordChange}
+            value={password}
+            required
+          />
         </div>
-    );
+        <div className="Login__rememberForget">
+          <label htmlFor="remember_me">
+            <input type="checkbox" name="remember_me" />
+            Remember me
+          </label>
+          <Link className="Login__rememberForget__Link" to="/recover">
+            <div>Olvide mi Contraseña</div>
+          </Link>
+        </div>
+        <div>
+          <Link to="/admin">
+            <button
+              className="Login__submit"
+              type="submit"
+              disabled={!email || !password}
+              onSubmit={handleSubmit}
+            >
+              Inicia Sesión
+            </button>
+          </Link>
+        </div>
+        <div className="Login__register">
+          <span>¿No tienes cuenta?</span>
+          <Link className="Login__register__Link" to="/sign-in">
+            <div>Regístrate</div>
+          </Link>
+        </div>
+        <div>
+          <button className="Login__Google">Continua con Google</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
